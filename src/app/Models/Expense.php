9094223e94e292b686dda52cfb41b545173a23bf
yakhlafhoussam <yakhlafhoussam@.com>
2026-02-24 23:34:54 +0000

    Config model expense

---
+++ src/app/Models/Expense.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Expense extends Model
+{
+    protected $fillable = [
+        'colocation_id',
+        'payer_id',
+        'categorie_id',
+        'title',
+        'amount',
+    ];
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function payer()
+    {
+        return $this->belongsTo(User::class, 'payer_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Categorie::class);
+    }
+}
