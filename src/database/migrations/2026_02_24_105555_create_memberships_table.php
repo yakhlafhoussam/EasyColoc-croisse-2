6d4b778bb5e9e7b5b91dd32bce7ae2a5662b3628
yakhlafhoussam <yakhlafhoussam@.com>
2026-02-24 12:30:14 +0000

    Add memberships table

---
+++ src/database/migrations/2026_02_24_105555_create_memberships_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('memberships', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('colocation_id')->constrained()->onDelete('cascade');
+            $table->enum('role', [0, 1]);
+            $table->timestamp('join_at');
+            $table->timestamp('left_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('memberships');
+    }
+};
