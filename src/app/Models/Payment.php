ba667043522b346d49a1244691bc202f28d50cd9
yakhlafhoussam <yakhlafhoussam@.com>
2026-02-24 23:35:54 +0000

    Config model payment

---
+++ src/app/Models/Payment.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    protected $fillable = [
+        'colocation_id',
+        'payer_id',
+        'receiver_id',
+        'title',
+        'amount',
+        'payment_date',
+    ];
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function payer()
+    {
+        return $this->belongsTo(User::class, 'payer_id');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
+}
