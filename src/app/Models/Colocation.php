dd3e59385d31ed5fddb7b866030f6e743d541cce
yakhlafhoussam <yakhlafhoussam@.com>
2026-02-24 23:34:37 +0000

    Config model colocation

---
+++ src/app/Models/Colocation.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Colocation extends Model
+{
+    protected $fillable = [
+        'name',
+        'max_members',
+        'status',
+        'country',
+        'city',
+        'desc',
+    ];
+
+    public function memberships()
+    {
+        return $this->hasMany(Membership::class);
+    }
+
+    public function categories()
+    {
+        return $this->hasMany(Categorie::class);
+    }
+
+    public function expenses()
+    {
+        return $this->hasMany(Expense::class);
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class);
+    }
+
+    public function invitations()
+    {
+        return $this->hasMany(Invitation::class);
+    }
+
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class);
+    }
+}
