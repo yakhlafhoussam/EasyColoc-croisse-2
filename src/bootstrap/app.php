528365bbb598c4ca802ac812ab7d86017b5c5e35
yakhlafhoussam <yakhlafhoussam@.com>
2026-02-24 21:47:48 +0000

    Add middleware check

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'permission' => App\Http\Middleware\CheckPermission::class,
+            'auth.custom' => App\Http\Middleware\AuthMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
