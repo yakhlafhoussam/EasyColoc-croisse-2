a4e4aa7000753b224333683e9bfd5cf09c8a344d
yakhlafhoussam <yakhlafhoussam@.com>
2026-02-24 23:36:11 +0000

    Config model rating

---
+++ src/app/Models/Rating.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Rating extends Model
+{
+    protected $fillable = [
+        'to_user_id',
+        'stars',
+        'comment',
+    ];
+
+    public function fromUser()
+    {
+        return $this->belongsTo(User::class, 'from_user_id');
+    }
+
+    public function toUser()
+    {
+        return $this->belongsTo(User::class, 'to_user_id');
+    }
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+}
